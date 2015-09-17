ae48069787189f94356553e4f6e799798dd6220c
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-09-17 13:47:41 -0500

    Missing semicolon in titleize helper

--- templates/app/helpers/titleize.js
+++ templates/app/helpers/titleize.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Helper.helper((params) => {
   const sentence = params[0] || "";
-  const titleized = sentence.w().map((word) => { return word.capitalize() }).join();
+  const titleized = sentence.w().map((word) => { return word.capitalize(); }).join();
   return `${titleized}`;
 });
