9f3c987bfd50099a06936300cddf0b3a66365008
John-Henry Liberty <johnhenry.liberty@gmail.com>
2016-03-06 01:29:39 -0600

    adds ember-cli-mirage

--- templates/config/environment.js
+++ templates/config/environment.js
@@ -32,10 +32,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
     ENV.apiURL = "http://localhost:1776";
     ENV['ember-cli-mirage'] = { enabled: true };
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
     ENV.APP.rootElement = '#ember-testing';
